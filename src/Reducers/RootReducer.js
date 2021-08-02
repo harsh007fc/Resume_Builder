b4c2f3443cb6bf71fcd274e6980491db974c76bf
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-08-02 11:12:19 +0530

    added upto auth reducer

--- src/Reducers/RootReducer.js
+++ src/Reducers/RootReducer.js
@@ -4,13 +4,25 @@ import educationReducer from './EducationReducer';
 import documentReducer from './DocumentReducers';
 import {firestoreReducer} from 'redux-firestore'
 import {firebaseReducer} from 'react-redux-firebase'
+import authReducer from './AuthReducer';
+import initialState from './InitialState.json'
+import * as actionTypes from '../Actions/ActionTypes'
 
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
     firestore:firestoreReducer,
     firebase:firebaseReducer,
     contactSection:contactReducer,
     educationSection:educationReducer,
     document:documentReducer,
+    auth:authReducer
 })
+
+
+const rootReducer = (state=initialState,action) =>{
+    if(action.type === actionTypes.SIGN_OUT){
+        state=undefined;
+    }
+    return appReducer(state,action)
+}
 export default rootReducer;
